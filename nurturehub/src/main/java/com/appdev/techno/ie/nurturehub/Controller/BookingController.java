85034b082c44d0de653e0e4e22c965cc25f55ec1
Juspher123 <juspherpatena@gmail.com>
2023-12-03 18:47:28 +0800

    Booking Logic

--- nurturehub/src/main/java/com/appdev/techno/ie/nurturehub/Controller/BookingController.java
+++ nurturehub/src/main/java/com/appdev/techno/ie/nurturehub/Controller/BookingController.java
@@ -1,5 +1,52 @@
 package com.appdev.techno.ie.nurturehub.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appdev.techno.ie.nurturehub.Entity.BookingEntity;
+import com.appdev.techno.ie.nurturehub.Service.BookingService;
+
+
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/booking")
 public class BookingController {
 
+	@Autowired
+	BookingService bserv;
+	
+	@PostMapping("/insertBooking")
+	public BookingEntity insertBooking(@RequestBody BookingEntity booking) {
+		return bserv.insertBooking(booking);
+	}
+	
+	@GetMapping("/getAllBookings")
+	public List<BookingEntity>getAllBookings(){
+		return bserv.getAllBookings();
+	}
+	
+	@PutMapping("/updateBooking")
+	public BookingEntity updateBooking(@RequestParam int bid,@RequestBody BookingEntity newBookingDetails){
+		return bserv.updateBooking(bid, newBookingDetails);
+	}
+	
+	@DeleteMapping("/deleteBooking/{bid}")
+	public String deleteBooking(@PathVariable int bid) {
+		return bserv.deleteBooking(bid);
+	}
+	
+	
 }
--- nurturehub/src/main/java/com/appdev/techno/ie/nurturehub/Service/BookingService.java
+++ nurturehub/src/main/java/com/appdev/techno/ie/nurturehub/Service/BookingService.java
@@ -1,5 +1,63 @@
 package com.appdev.techno.ie.nurturehub.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appdev.techno.ie.nurturehub.Entity.BookingEntity;
+import com.appdev.techno.ie.nurturehub.Repository.BookingRepository;
+
+
+@Service
 public class BookingService {
 
+	@Autowired
+	BookingRepository repo;
+	
+	public BookingEntity insertBooking(BookingEntity booking) {
+		return repo.save(booking);
+	}
+	
+	
+	public List<BookingEntity> getAllBookings(){
+		return repo.findAll();
+	}
+	
+	@SuppressWarnings("finally")
+	public BookingEntity updateBooking(int bid, BookingEntity newBookingDetails) {
+		BookingEntity booking = new BookingEntity();
+		try {
+			//1.)search the id number of student that will be updated
+			booking = repo.findById(bid).get();
+			
+		    //2.) update the record
+			booking.setCaregiver(newBookingDetails.getCaregiver());
+			booking.setRecipient(newBookingDetails.getRecipient());
+			booking.setStart_date(newBookingDetails.getStart_date());
+			booking.setEnd_date(newBookingDetails.getEnd_date());
+			booking.setIsTerminated(newBookingDetails.getIsTerminated());
+			
+		}catch(NoSuchElementException ex) {
+			throw new NoSuchElementException("Student "+ bid + " does not exist!");
+		}finally {
+			return repo.save(booking);
+		}
+	}
+	
+	public String deleteBooking(int bid) {
+		String msg = "";
+		
+		if(repo.findById(bid) != null) {
+			repo.deleteById(bid);
+			msg = "Booking " + bid + " is successfully deleted!";
+		}
+		else
+			msg = "Booking " + bid + " does not exist";
+		return msg;
+		
+	}
+	
+	
 }
